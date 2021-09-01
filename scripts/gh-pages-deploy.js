a0512396de6961b35550e2b22ff3b47e29761e1d
XessWaffle <varma.vatsal@gmail.com>
2021-09-01 16:17:38 -0400

    Check

---
+++ scripts/gh-pages-deploy.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+
+/* 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 */
+
+const execa = require("execa");
+const fs = require("fs");
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    //await execa("del", ["/f", "/s", "/q", folderName])
+    //await execa("rmdir", ["/s","/q", folderName]);
+    await execa("git", ["checkout", "-f", "main"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed, check your settings");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
